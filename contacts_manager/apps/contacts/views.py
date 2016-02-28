1b8959adfad3abb3bc2fff53c55975e4951046a3
Eric Fries <ericfries@Erics-MacBook-Air.local>
2016-02-28 12:13:53 -0500

    make linter happy

--- contacts_manager/apps/contacts/views.py
+++ contacts_manager/apps/contacts/views.py
@@ -5,26 +5,29 @@ from django.core.urlresolvers import reverse
 from .forms import PersonForm
 from .models import Person
 
+
 # Create your views here.
 def index(request):
-	if request.method == 'GET':
-		people = Person.objects.all()
-		context = {'people': people}
-		return render(request, 'contacts/index.html', context)
+    if request.method == 'GET':
+        people = Person.objects.all()
+        context = {'people': people}
+        return render(request, 'contacts/index.html', context)
+
 
 def detail(request, person_id):
-	person = get_object_or_404(Person, pk=person_id)
-	context = {'person': person}
-	return render(request, 'contacts/detail.html', context)
+    person = get_object_or_404(Person, pk=person_id)
+    context = {'person': person}
+    return render(request, 'contacts/detail.html', context)
+
 
 def create(request):
-	form = PersonForm()
+    form = PersonForm()
 
-	if request.method == 'POST':
-		form = PersonForm(request.POST)
-		if form.is_valid():
-			person = form.save()
-			import ipdb; ipdb.set_trace()
-		return HttpResponseRedirect(reverse('contacts:detail', args=(person.id,)))
+    if request.method == 'POST':
+        form = PersonForm(request.POST)
+        if form.is_valid():
+            person = form.save()
+        return HttpResponseRedirect(reverse(
+            'contacts:detail', args=(person.id,)))
 
-	return render(request, 'contacts/create.html', {'form': form})
+    return render(request, 'contacts/create.html', {'form': form})
